570133d396113b04d067aec23013c40726d75d42
lee92hs-ops <lee.92hs@gmail.com>
2026-02-16 15:58:27 +0900

    Update Main.java

--- print8/question06/Main.java
+++ print8/question06/Main.java
@@ -7,19 +7,19 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		//1
+
 		Scanner sc = new Scanner(System.in);
-		//2
+
 		System.out.print("乱数を作成する数を指定してください:");
 		int n = sc.nextInt();
 		ArrayList<Integer> list = getListData(n);
-		//3
+
 		showListData(list);
 	}
 	public static ArrayList<Integer> getListData(int n){
-		//1.	ArrayListを変数名listとし宣言する
+
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		//2
+
 		Random rand = new Random();
 		for(int i=0;i<n;i++) {
 			list.add(rand.nextInt(100));
